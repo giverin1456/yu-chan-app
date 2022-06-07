3ef10069872f40d0a14f63377cafcfe1502cb752
giverin1456 <give.taiki1456@gmail.com>
2022-06-07 22:33:06 +0900

    予約システム

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,40 @@ class ItemsController < ApplicationController
     @admin = current_admin
     @now = Date.today.month
     @outpatient = Outpatient.all.order(start_time: :DESC)
-
     @item = @outpatient.page(params[:page]).per(15)
+
+    @reservations = Item.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
   end
+
+  def new
+    @reservation = Item.new
+    @day = params[:day]
+    @time = params[:time]
+    @start_time = DateTime.parse(@day + " " + @time + " " + "JST")
+  end
+
+  def show
+    @reservation = Item.find(params[:id])
+  end
+
+  def create
+    @reservation = Item.new(reservation_params)
+    if @reservation.save
+      redirect_to item_path @reservation.id
+    else
+      render :new
+    end
+  end
+
 
   def information
     @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
     @orders = User.where(created_at: @month.all_month)
     @now = Time.now.strftime('%Y%m%d')
-    
+  end
+
+  private
+  def reservation_params
+    params.require(:item).permit(:day, :time, :user_id, :start_time)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_15_093708) do
+ActiveRecord::Schema.define(version: 2022_06_07_115353) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2022_05_15_093708) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "day", null: false
+    t.string "time", null: false
+    t.bigint "user_id", null: false
+    t.datetime "start_time", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "outpatients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
